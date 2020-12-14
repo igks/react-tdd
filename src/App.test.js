09f82277edfcce6cfb06a93de89627368b96128c
I Gusti Kade <sugiantara.igk@gmail.com>
2020-12-14 15:43:38 +0700

    react test with jest and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from "enzyme";
+import Counter from "./Counter";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Counter Testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Counter />);
+  });
+
+  test("Render the title of the counter", () => {
+    expect(wrapper.find("h1").text()).toContain("This is counter app");
+  });
+
+  test("Render a button with text of increment", () => {
+    expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+  });
+
+  test("Render a button with text of decrement", () => {
+    expect(wrapper.find("#decrement-btn").text()).toBe("Decrement");
+  });
+
+  test("Render initial counter value in a div", () => {
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
+
+  test("Render the click event of increment button and increment counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+  });
+
+  test("Render the click event of decrement button and decrement counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+    wrapper.find("#decrement-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
+
+  test("Prevent minus value of counter when decrement button was click", () => {
+    wrapper.find("#decrement-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
 });
---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>This is counter app</h1>
+      <div id="counter-value">{counter}</div>
+      <button id="increment-btn" onClick={() => setCounter(counter + 1)}>
+        Increment
+      </button>
+      <button
+        id="decrement-btn"
+        onClick={() => counter > 0 && setCounter(counter - 1)}
+      >
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
